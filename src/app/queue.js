478ce8c93ab5078ca42c41e9c1e018de58c1e25f
combi2k2 <cumbee2k2@gmail.com>
2025-12-31 21:36:28 +0100

    feat: queue data structure for later algorithms

---
+++ src/app/queue.js
@@ -0,0 +1,24 @@
+export default class Queue {
+    constructor() {
+        this.data = [];
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    push(value) {
+        this.data.push(value);
+        this.tail++;
+    }
+
+    pop() {
+        return this.data[this.head++];
+    }
+
+    empty() {
+        return this.head >= this.tail;
+    }
+
+    size() {
+        return this.tail - this.head;
+    }
+}
