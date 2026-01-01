008cbe0daf07494af66c0e4514167a291a1627f0
combi2k2 <cumbee2k2@gmail.com>
2026-01-01 08:51:10 +0100

    fix: loosen esm strict to enable commonJS to be used

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack(config, { isServer }) {
+    // Allow WASM loading
+    config.experiments = {
+      ...(config.experiments || {}),
+      asyncWebAssembly: true,
+    };
+
+    // Prevent client-side fs/path crashes
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+
+    return config;
+  },
   turbopack: {}
 };
 
